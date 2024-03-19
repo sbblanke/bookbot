85bde11a979e0f3eccc5b90c853227ed0cb9ef4c
sbblanke <sidneyblankenship1@gmail.com>
2024-03-19 00:58:47 -0400

    added a word counter

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_num_words(text): 
+    words = text.split()
+    return len(words)
+
 def main():
-    with open('/home/sbblanke/workspace/github.com/sbblanke/bookbot/books/frankenstein.txt') as f:
-        file_contents = f.read()
-   
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
 
 main()
