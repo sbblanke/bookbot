38e9b31c2ee816a32d4d0bc94eb3b72a6740abd3
sbblanke <sidneyblankenship1@gmail.com>
2024-03-19 00:33:13 -0400

    added frankenstein.txt, .gitignore files, and text in main.py file to print the contents of frankenstein.txt

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+def main():
+    with open('/home/sbblanke/workspace/github.com/sbblanke/bookbot/books/frankenstein.txt') as f:
+        file_contents = f.read()
+   
+    print(file_contents)
+
+main()
