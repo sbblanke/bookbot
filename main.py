9ae112a7df13eb331329045329bc33a2964de58b
sbblanke <sidneyblankenship1@gmail.com>
2024-03-19 23:22:43 -0400

    added a function to count how many of each letter is present in text and imported defaultdict

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
@@ -10,6 +12,16 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    chars_count = count_letters(text)
+    print(chars_count)
+
+def count_letters(text):
+    count_of_letters = defaultdict(int)
+    lower_case_string = text.lower()
+    for i in lower_case_string:
+        if i.isalpha():
+            count_of_letters[i] +=1
+    return count_of_letters
+            
 
 main()
