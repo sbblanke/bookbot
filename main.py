7ffe9d98ba0ae82b92b2e18a0fba4d129d1b2b0e
sbblanke <sidneyblankenship1@gmail.com>
2024-03-20 10:40:27 -0400

    update main.py with the sorting function, added text to make the report legible, project complete

--- main.py
+++ main.py
@@ -13,7 +13,15 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_count = count_letters(text)
-    print(chars_count)
+    sorted_chars = sort_dict(chars_count)
+    
+    print(f" --- Begin Report of {book_path} --- ")
+    print(f"{get_num_words(text)} words found in the document")
+    print()
+    for char_dict in sorted_chars:
+        print(f"The {char_dict['letter']} character was found {char_dict['num']} times")
+    print()
+    print("--- End Report ---")
 
 def count_letters(text):
     count_of_letters = defaultdict(int)
@@ -23,5 +31,15 @@ def count_letters(text):
             count_of_letters[i] +=1
     return count_of_letters
             
+def sort_dict(count_of_letters):
+    list_of_dictionaries = []
+    for key, value in count_of_letters.items():
+        new_dict = {"letter": key, "num": value}
+        list_of_dictionaries.append(new_dict)
+    list_of_dictionaries.sort(reverse=True, key=extract_num)
+    return list_of_dictionaries
+
+def extract_num(dictionary):
+    return dictionary["num"]
 
 main()
